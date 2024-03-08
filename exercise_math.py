6bab1540b403d198084caf003431039caaa1fd47
DHipo <bautidhipolito@gmail.com>
2024-03-08 14:13:10 -0300

    ejercicio 2

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,16 @@
 def math():
     a = 57
     b = 7
+
+    results = [
+        a + b,
+        a - b,
+        a * b,
+        (a + b) / 2,
+        int(a / b),
+        int(a % b),
+        a / b,
+    ]
+
+    for e in results:
+        print(e)
